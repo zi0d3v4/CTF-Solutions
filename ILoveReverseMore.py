ff301da884b9377b37b3b76ef54d1208987df0b9
zi0d3v4 <75640323+zi0d3v4@users.noreply.github.com>
2021-09-27 07:19:54 +0300

    Create ILoveReverseMore.py

---
+++ ILoveReverseMore.py
@@ -0,0 +1,12 @@
+#Saudi Arabia National Cybersecurity CTF 2021 - ILoveReverse
+#python3 code 
+
+key="E`am]Ht`Ws"
+ans=[]
+
+for i in range(len(key)):
+    a=hex(ord(key[i])+(6-i))
+    ans.append(a[2:])
+
+print(bytes.fromhex(''.join(ans)).decode('utf-8'))
+exit()
